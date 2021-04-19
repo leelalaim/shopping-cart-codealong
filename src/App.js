7732bacca25ebe9fab33aa8dce854de403775d54
Laima Duhovnaja <laima@laimas-mbp.mynet>
2021-04-19 15:09:49 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import {Provider} from 'react-redux'
 import { Products } from 'components/Products'
 import { Cart } from 'components/Cart'
 
+import { cart } from './reducers/cart'
+import { products } from './reducers/products'
+import { combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+const reducer = combineReducers({
+  cart: cart.reducer,
+  products: products.reducer
+})
+
+const store = configureStore({ reducer })
+
 export const App = () => (
-  <>
+  <Provider store={store}>
     <Cart />
     <Products />
-  </>
+  </Provider>
 )
